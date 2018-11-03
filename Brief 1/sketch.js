97ab277270889e9eb6488b3c1456a6e6501a04c5
apple <apple@apples-MacBook-Pro.local>
2018-11-03 22:37:26 +0000

    User selected dot amount added, traces added for all new dots

--- Brief 1/sketch.js
+++ Brief 1/sketch.js
@@ -1,67 +1,65 @@
 //asks user how many circles they want on the screen
 var promptnum = prompt("How many circles on the screen?");
-
-//creates the first ball function
-var ball = {
-    x: 300,
-    y: 200,
-    xspeed: 4,
-    yspeed: -3,
-    history: []
-}
-
-//creates the second ball function
-var ball2 = {
-  x:300,
-  y:200,
-  xspeed: 4,
-  yspeed: -3,
-  history: []
-}
+var points = [];
 
 //setup for canvas
 function setup(){
   createCanvas(600, 400);
-  frameRate(30);
+  background(0, 100, 255);
+  for (var i = 0; i < promptnum; i++) {
+    points[i] = new dots;
+  }
 }
 
 //draw on canvas
 function draw() {
-  background(0, 0, 0, 127);
-  strokeWeight(0);
+  noStroke();
 
-  //adds or subtracts 2 from the speed of the circles every loop for random movements
-  ball.x += random(-4, 4);
-  ball.y += random(-4, 4);
-  ball2.x += random(-4, 4);
-  ball2.x += random(-4, 4);
-  //creates the circles
-  ellipse(ball.x, ball.y, 10, 10);
-  ellipse(ball2.x, ball2.y, 10, 10);
-
-  var v = createVector(ball.x, ball.y);
-  ball.history.push(v);
-  console.log(v);
-  for (var i = 0; i < ball.history.length; i++) {
-    var pos = ball.history[i];
-    ellipse(pos.x, pos.y, 5, 5)
-    fill(random(70, 140), random(70, 140), random(70, 140));
-  }
-
-  //changes direction of ball when it hits the side of the canvas
-  if (ball.x > width || ball.x < 0) {
-    ball.xspeed = ball.xspeed * -1;
-  }
-
-  if (ball.y > height || ball.y < 0) {
-    ball.yspeed = ball.yspeed * -1;
-  }
-
-  if (ball2.x > width || ball2.x < 0) {
-    ball2.xspeed = ball2.xspeed * -1;
-  }
-
-  if (ball2.y > height || ball2.y < 0) {
-    ball2.yspeed = ball2.yspeed * -1;
+  //calls functions to draw and then move point
+  for (var i = 0; i < promptnum; i++) {
+    points[i].drawPoint();
+    points[i].movePoint();
   }
 }
+//function to position and calibrate dots
+function dots(){
+  this.x = width/2;
+  this.y = height/2;
+  this.xspeed;
+  this.yspeed;
+  /*
+  this.history = [];
+  var t = createVector(this.x, this.y)
+  this.history.push(t);
+  for (var t = 0; t < this.history.length; t++) {
+    var pos = this.history[t];
+    ellipse(pos.x, pos.y, 8, 8)
+    fill(random(70, 140), random(70, 140), random(70, 140));
+  }*/
+
+    //function to draw a new dot
+    this.drawPoint = function(){
+    fill(random(0, 1), random(200, 255), random(100, 140));
+    noStroke();
+    //creating ellipse with x and y from movepoint function
+    ellipse(this.x, this.y, 2, 2);
+    }
+
+    //function to moving x and y co-ordinates
+    this.movePoint = function(){
+    //choosing random x and y speed
+    this.xspeed = random(-2, 2);
+    this.yspeed = random(-2, 2);
+    //adding random speed to both x and y co-ordinates
+    this.x += this.xspeed;
+    this.y += this.yspeed;
+      console.log(this.x, this.y)
+    //changes direction of ball when it hits the side of the canvas
+      if (this.x > width || this.x < 0) {
+      this.xspeed = this.xspeed * -1;
+      }
+      if (this.y > height || this.y < 0) {
+      this.yspeed = this.yspeed * -1;
+      }
+    }
+  }
