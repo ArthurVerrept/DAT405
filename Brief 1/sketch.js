b5f490a9ebfde75f88392654f7f3f93e65928b9b
apple <apple@apples-MacBook-Pro.local>
2018-11-03 19:27:16 +0000

    adding trail to one of the circles

--- Brief 1/sketch.js
+++ Brief 1/sketch.js
@@ -3,62 +3,49 @@ var promptnum = prompt("How many circles on the screen?");
 
 //creates the first ball function
 var ball = {
-  x: 300,
-  y: 200,
-  xspeed: 4,
-  yspeed: -3
+    x: 300,
+    y: 200,
+    xspeed: 4,
+    yspeed: -3,
+    history: []
 }
 
 //creates the second ball function
 var ball2 = {
-  x:200,
-  y:100,
+  x:300,
+  y:200,
   xspeed: 4,
-  yspeed: -3
+  yspeed: -3,
+  history: []
 }
 
 //setup for canvas
 function setup(){
   createCanvas(600, 400);
+  frameRate(30);
 }
 
 //draw on canvas
 function draw() {
-  background(0);
-  stroke(255);
-  //sets thickness of circle
-  strokeWeight(4);
-  //makes circle hollow
-  noFill();
+  background(0, 0, 0, 127);
+  strokeWeight(0);
 
   //adds or subtracts 2 from the speed of the circles every loop for random movements
-  ball.xspeed = ball.xspeed + random(-2, 2);
-  ball.yspeed = ball.yspeed + random(-2, 2);
-  ball2.xspeed = ball2.xspeed + random(-2, 2);
-  ball2.yspeed = ball2.yspeed + random(-2, 2);
-
-  //create user input amount of circles of circles
-  /*
-  for (var i = 0; i <= promptnum; i++) {
-  }
-  */
-
+  ball.x += random(-4, 4);
+  ball.y += random(-4, 4);
+  ball2.x += random(-4, 4);
+  ball2.x += random(-4, 4);
   //creates the circles
-  ellipse(ball.x, ball.y, 24, 24);
-  ellipse(ball2.x, ball2.y, 24, 24);
+  ellipse(ball.x, ball.y, 10, 10);
+  ellipse(ball2.x, ball2.y, 10, 10);
 
-  //resets speed of circle once it reaches 5
-  if(ball.xspeed > 5 || ball.xspeed < -5){
-    ball.xspeed = random(-2, 2);
-  }
-  if(ball.yspeed > 5 || ball.yspeed < -5){
-    ball.yspeed = random(-2, 2);
-  }
-  if(ball2.xspeed > 5 || ball2.xspeed < -5){
-    ball2.xspeed = random(-2, 2);
-  }
-  if(ball2.yspeed > 5 || ball2.yspeed < -5){
-    ball2.yspeed = random(-2, 2);
+  var v = createVector(ball.x, ball.y);
+  ball.history.push(v);
+  console.log(v);
+  for (var i = 0; i < ball.history.length; i++) {
+    var pos = ball.history[i];
+    ellipse(pos.x, pos.y, 5, 5)
+    fill(random(70, 140), random(70, 140), random(70, 140));
   }
 
   //changes direction of ball when it hits the side of the canvas
@@ -77,11 +64,4 @@ function draw() {
   if (ball2.y > height || ball2.y < 0) {
     ball2.yspeed = ball2.yspeed * -1;
   }
-
-  //increases x and y by the selected speed
-  ball.x = ball.x + ball.xspeed;
-  ball.y = ball.y + ball.yspeed;
-  ball2.x = ball2.x + ball2.xspeed;
-  ball2.y = ball2.y + ball2.yspeed;
-
 }
