a4a156a1bb5ed944c6b2337cfb1b50474be71dd7
apple <apple@apples-MacBook-Pro.local>
2018-11-16 17:34:22 +0000

    Added moving fill with a varying opacity based on the height of y axis

--- Perlin/sketch.js
+++ Perlin/sketch.js
@@ -2,21 +2,22 @@ var start = 0;
 
 function setup(){
   createCanvas(594, 841);
-  background(100);
 }
 
 function draw(){
-  noFill();
+  background(100);
+  //noFill();
   beginShape();
     var xoff = start;
     for (var x = 0; x < width; x++) {
       y = map(noise(xoff), 0, 1, 0, height);
-      console.log(y);
-      stroke(255);
+      var colour = map(y, 0, height, 0, 255);
+      console.log(colour);
+      fill(0, 0, 0, colour);
       vertex(x, y);
       xoff += 0.01;
-
     }
     endShape();
+    stroke(255);
     start += 0.01;
 }
