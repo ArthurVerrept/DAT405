5e565bab5f6066629571783931f76edbc116b0f1
apple <apple@apples-MacBook-Pro.local>
2018-11-17 18:14:36 +0000

    flow field based on TWO_PI to generate angle

--- FlowField/sketch.js
+++ FlowField/sketch.js
@@ -1,33 +1,44 @@
 //set up increment amount
 var inc = 0.1;
+//set up scale of flow field
+var scl = 20;
+//set up col and rows
+var cols, rows;
+
+var zoff = 0;
 
 function setup(){
-  createCanvas(594, 841);
-    background(100);
+  createCanvas(600, 800);
+  //background(100);
+  //divides amount of pixels in width and height by scl to group pixels
+  cols = floor(width / scl);
+  rows = floor(height/ scl);
 }
 
 function draw(){
+  background(100);
   //resets y offset to 0 in noise after evey column
   var yoff = 0;
-   loadPixels();
-    for (var y = 0; y < height; y++) {
+  //goes through each row
+    for (var y = 0; y < rows; y++) {
       //for every row we reset xoff in noise at 0
       var xoff = 0;
-      for (var x = 0; x < width; x++) {
-        //finds location in pixels array
-        var index = (x + y * width) * 4;
-        //sets variable c to the map of xoff and yoff returning a value between 0-255
-        var c = map(noise(xoff, yoff), 0, 1, 0, 255);
-        //sets pixel to c for each colour RGB
-        pixels[index] = c;
-        pixels[index+1] = c;
-        pixels[index+2] = c;
-        pixels[index+3] = 255;
-        //for every pixel in row xoff is incremented by inc
+      //goes through each column
+      for (var x = 0; x < cols; x++) {
+        //var index = (x + y * width) * 4;
+        //
+        var angle = noise(xoff, yoff, zoff) * TWO_PI;
+        var v = p5.Vector.fromAngle(angle);
+        stroke(255);
+        push();
+        translate(x * scl, y * scl)
+        rotate(v.heading());
+        line(0, 0, scl, 0);
+        pop();
         xoff += inc;
       }
       //for evey new column yoff in incremented by inc
       yoff += inc;
+      zoff += 0.0005;
     }
-    updatePixels();
 }
