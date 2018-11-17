c747e34350260652ae49e802a0c60f0957ac4919
apple <apple@apples-MacBook-Pro.local>
2018-11-17 15:59:40 +0000

    using loadpixel understanding in perlin to create 2D pelin noise

---
+++ 2DPerlin/sketch.js
@@ -0,0 +1,24 @@
+var start = 0;
+
+function setup(){
+  createCanvas(594, 841);
+}
+
+function draw(){
+  background(100);
+  //noFill();
+  beginShape();
+    var xoff = start;
+    for (var x = 0; x < width; x++) {
+      y = map(noise(xoff), 0, 1, 0, height);
+      var colour = map(y, 0, height, 0, 255);
+      var scolour = map(y, 0, height, 0, 255);
+      stroke(scolour);
+      fill(0, 0, 0, colour);
+      vertex(x, y);
+      xoff += 0.01;
+    }
+    endShape();
+    stroke(255);
+    start += 0.01;
+}
