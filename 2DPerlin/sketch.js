5ade29fb4e7c4e95a46017dcbdcf7a9f4acb6daa
apple <apple@apples-MacBook-Pro.local>
2018-11-17 16:58:17 +0000

    2d perlin noise visualized

--- 2DPerlin/sketch.js
+++ 2DPerlin/sketch.js
@@ -1,24 +1,33 @@
-var start = 0;
+//set up increment amount
+var inc = 0.1;
 
 function setup(){
   createCanvas(594, 841);
+    background(100);
 }
 
 function draw(){
-  background(100);
-  //noFill();
-  beginShape();
-    var xoff = start;
-    for (var x = 0; x < width; x++) {
-      y = map(noise(xoff), 0, 1, 0, height);
-      var colour = map(y, 0, height, 0, 255);
-      var scolour = map(y, 0, height, 0, 255);
-      stroke(scolour);
-      fill(0, 0, 0, colour);
-      vertex(x, y);
-      xoff += 0.01;
+  //resets y offset to 0 in noise after evey column
+  var yoff = 0;
+   loadPixels();
+    for (var y = 0; y < height; y++) {
+      //for every row we reset xoff in noise at 0
+      var xoff = 0;
+      for (var x = 0; x < width; x++) {
+        //finds location in pixels array
+        var index = (x + y * width) * 4;
+        //sets variable c to the map of xoff and yoff returning a value between 0-255
+        var c = map(noise(xoff, yoff), 0, 1, 0, 255);
+        //sets pixel to c for each colour RGB
+        pixels[index] = c;
+        pixels[index+1] = c;
+        pixels[index+2] = c;
+        pixels[index+3] = 255;
+        //for every pixel in row xoff is incremented by inc
+        xoff += inc;
+      }
+      //for evey new column yoff in incremented by inc
+      yoff += inc;
     }
-    endShape();
-    stroke(255);
-    start += 0.01;
+    updatePixels();
 }
