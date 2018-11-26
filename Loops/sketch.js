c6eda66bfbe4573b76dda0ffccb9e28ca48b5702
apple <apple@apples-MacBook-Pro.local>
2018-11-26 11:55:36 +0000

    comments

--- Loops/sketch.js
+++ Loops/sketch.js
@@ -1,30 +1,23 @@
-//https://github.com/stavrosdidakis/DAT-GAD-405_2017
-//https://p5js.org
-
-//DAT405 / GAD405
-//01_ProjectSetup
-//In this example we see how to create a canvas, set and display
-//the frame rate of the rendering system, and use text to
-//display the frame count and rate on screen
-
-//Initialization function
+//sets sizes of squares
 let size = 50;
-let size2 = 30;
+
 function setup() {
+  //framerate
   frameRate(3);
   createCanvas(500, 500);
 }
 function draw(){
+  //loops through canvas size for each square
   for (var y = 0; y < 500; y+=50) {
     for (var x = 0; x < 500; x+=50) {
+      //fills outer squares a static colour
       fill(220, 220, 0);
+      //draws squares
       rect(x, y, size, size);
-    }
-  }
-  for (var y2 = 10; y2 < 500; y2+=50) {
-    for (var x2 = 10; x2 < 500; x2+=50) {
+      //fills inner square random colour
       fill(random(255), random(255), random(255));
-      rect(x2, y2, size2, size2);
+      //draws inner square
+      rect(x+10, y+10, size-20, size-20);
     }
   }
 }
